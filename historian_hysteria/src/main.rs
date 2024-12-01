1cbbca507b3dead7234e9c38e7ab04cf44269a0c
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2024-12-01 09:38:02 +0100

    historian_hysteria: add second part
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- historian_hysteria/src/main.rs
+++ historian_hysteria/src/main.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "part_two")] use std::collections::HashMap;
+
 fn build(input: String) -> (Vec<u64>, Vec<u64>)
 {
     let mut ret = (Vec::new(), Vec::new());
@@ -11,14 +13,15 @@ fn build(input: String) -> (Vec<u64>, Vec<u64>)
         }
     }
 
-    ret.0.sort();
-    ret.1.sort();
-
     ret
 }
 
-fn solve(input: &(Vec<u64>, Vec<u64>)) -> u64
+#[cfg(feature = "part_one")]
+fn solve(input: &mut (Vec<u64>, Vec<u64>)) -> u64
 {
+    input.0.sort();
+    input.1.sort();
+
     input
         .0
         .iter()
@@ -29,9 +32,31 @@ fn solve(input: &(Vec<u64>, Vec<u64>)) -> u64
         .sum()
 }
 
+#[cfg(feature = "part_two")]
+fn solve(input: &(Vec<u64>, Vec<u64>)) -> u64
+{
+    let mut left = HashMap::<u64, u64>::new();
+    let mut right = HashMap::<u64, u64>::new();
+
+    for (a, b) in input.0.iter().copied().zip(input.1.iter().copied()) {
+        let left_value = *left.entry(a).or_insert(0) + 1;
+        let right_value = *right.entry(b).or_insert(0) + 1;
+
+        left.insert(a, left_value);
+        right.insert(b, right_value);
+    }
+
+    input
+        .0
+        .iter()
+        .copied()
+        .map(|a| a * right.get(&a).unwrap_or(&0))
+        .sum()
+}
+
 fn main()
 {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let parsed = build(input);
-    println!("{:?}", solve(&parsed));
+    let mut parsed = build(input);
+    println!("{:?}", solve(&mut parsed));
 }
